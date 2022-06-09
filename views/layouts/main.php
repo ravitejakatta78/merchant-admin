a308b5ff963e250918d8221e7ec53d14cbc0c06c
LAPTOP-9BFQG462\qq <ravitejakatta78@gmail.com>
2022-06-09 05:32:07 -0700

    Changed favicon path

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -19,7 +19,7 @@ AppAsset::register($this);
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>SuperPilot Admin</title>
-    		<link rel="shortcut icon" href="<?php echo Url::base(); ?>/favicon-new.png" type="image/x-icon" />
+    		<link rel="shortcut icon" href="<?= Yii::$app->request->baseUrl.'/img/superpilot.png'?>" type="image/x-icon" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="all,follow">
